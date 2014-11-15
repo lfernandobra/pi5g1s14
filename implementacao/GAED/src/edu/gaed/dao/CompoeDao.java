88cac80cd1b8c7940d5de965cac82e71b5e4df05
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2014-11-15 16:21:57 +0000

    O que implementou?
    - Visualizar Boletim (Aluno)
    
    Em qual parte parou?
    
    - Visualizar Boletim (Responsavel)

--- implementacao/GAED/src/edu/gaed/dao/CompoeDao.java
+++ implementacao/GAED/src/edu/gaed/dao/CompoeDao.java
@@ -69,4 +69,55 @@ public class CompoeDao extends BaseDao{
 		
 		return composicao;
 	}
+	
+	public List<Compoe> obterComposicao(int idBoletim) {
+		Connection conn = null;
+		List<Compoe> composicao = new ArrayList<Compoe>();
+		
+		try {
+			conn = this.getConnection();
+			
+			String sql = "select c.ID_Boletim,b.Bimestre,c.ID_Disciplina,d.Nome_Disciplina,c.Nota,c.Faltas from Compoe c,Boletim b ,Disciplina d where b.ID_Boletim = c.ID_Boletim and c.ID_Disciplina = d.ID_Disciplina and b.ID_Boletim =?";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			
+			stmt.setInt(1, idBoletim);
+					
+			ResultSet resultado = stmt.executeQuery();
+			
+			while (resultado.next()) {
+				Boletim boletim = new Boletim();
+				boletim.setID(resultado.getInt("ID_Boletim"));
+				boletim.setBimestre(resultado.getInt("Bimestre"));
+				
+				Disciplina disciplina = new Disciplina();
+				disciplina.setID(resultado.getInt("ID_Disciplina"));
+				disciplina.setNome(resultado.getString("Nome_Disciplina"));
+				
+				Compoe compoe = new Compoe();
+				compoe.setBoletim(boletim);
+				compoe.setNota(resultado.getFloat("Nota"));
+				compoe.setFaltas(resultado.getInt("Faltas"));
+				compoe.setDisciplina(disciplina);					
+				
+				composicao.add(compoe);
+			}
+			
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}				
+			}
+			catch (Exception e) {
+				//do nothing
+			}
+		}
+	
+		return composicao;
+	}
 }
