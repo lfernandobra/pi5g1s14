b9c018f87ae1c3b2faa4ec8535721e48f439710f
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-04-02 03:23:23 +0000

    - Alterações nas classes

--- GAED_Final/src/edu/gaed/vo/Responsavel.java
+++ GAED_Final/src/edu/gaed/vo/Responsavel.java
@@ -1,7 +1,11 @@
 package edu.gaed.vo;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
 
 @Entity
@@ -10,6 +14,9 @@ public class Responsavel extends Usuario{
 	
 	@Column(name="adimplente", nullable=false)
 	private boolean adimplente;
+	
+	@OneToMany
+	private Set<Aluno> aluno = new HashSet<Aluno>(0);
 	
 	public Responsavel() {
 		super();
