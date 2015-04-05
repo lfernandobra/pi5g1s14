2def39e74d5eadae3de4089eac647170eac47f9f
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-04-05 22:24:37 +0000

    - CRUD entidade UF criado com sucesso (teste) , método será replicado para outras entidades

--- GAED/src/edu/gaed/bean/UFBean.java
+++ GAED/src/edu/gaed/bean/UFBean.java
@@ -3,60 +3,56 @@ package edu.gaed.bean;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
-import javax.persistence.EntityManager;
-import javax.persistence.Query;
 
-import edu.gaed.controladores.JPAUtil;
+import edu.gaed.dao.UFDao;
 import edu.gaed.vo.UF;
 
 
 @ManagedBean(name="UFBean")
 public class UFBean {
+	
 	private UF uf = new UF();
 	private List<UF> ufs;
-	
-	//getter e setter
-
-
-	public UF getUf() {
+    
+	private UFDao dao = new UFDao();
+    
+    public UF getUf() {
 		return uf;
 	}
+
 
 	public void setUf(UF uf) {
 		this.uf = uf;
 	}
+
+
+	public UFDao getDao() {
+		return dao;
+	}
+
+
+	public void setDao(UFDao dao) {
+		this.dao = dao;
+	}	
+
+	public void salvar(){
+        dao.add(uf);
+    }
 	
-	/*
+	public List<UF> lista() {
+		return dao.lista();
+	}
+	
+	public void excluir(UF uf){
+		dao.excluir(uf);
+	}
+
 	public List<UF> getUfs() {
 		return ufs;
 	}
-	*/
+
 	public void setUfs(List<UF> ufs) {
 		this.ufs = ufs;
 	}
 
-	
-	public void salva(UF uf) {
-		EntityManager em = JPAUtil.getEntityManager();
-		em.getTransaction().begin();
-		em.persist(uf);
-		em.getTransaction().commit();
-		em.close();
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<UF> getUfs() {
-		
-		if(this.ufs == null) {
-			EntityManager em = JPAUtil.getEntityManager();
-			Query q = em.createQuery("select uf from UF uf",UF.class);
-			this.ufs= q.getResultList();
-			em.close();
-		}	
-		return ufs;
-	
-	}
-	
-
-	
 }
