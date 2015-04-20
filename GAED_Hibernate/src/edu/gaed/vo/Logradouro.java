1db0c27d98c883f13feed703ec6b4696ca157524
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-04-20 17:45:10 +0000

    - implementação teste do auto-complete de acordo com o tutorial : http://codepianist.com/java-ee/primefaces-autocomplete-com-converter/ Cadastro de usuarios a ser feito.

--- GAED_Hibernate/src/edu/gaed/vo/Logradouro.java
+++ GAED_Hibernate/src/edu/gaed/vo/Logradouro.java
@@ -61,6 +61,13 @@ public class Logradouro implements Serializable{
 		this.bairro = bairro;
 	}
 	
+	public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+	
 	@Override
     public boolean equals(Object obj) {
         if (this == obj)
