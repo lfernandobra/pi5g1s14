6ef370b524bca08f29bb60e874609a22ecec00dd
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-05-06 17:04:56 +0000

    - Ajustes UFDao e uf.xhtml

--- trunk/GAED_Hibernate/src/edu/gaed/dao/UFDao.java
+++ trunk/GAED_Hibernate/src/edu/gaed/dao/UFDao.java
@@ -6,68 +6,63 @@ import edu.gaed.vo.UF;
 
 import org.hibernate.Criteria;
 
-
 import org.hibernate.Session;
 
 import edu.gaed.util.HibernateUtil;
- 
+
 public class UFDao {
- 
-	private Session session; 
- 
-	public void inserir(UF uf){
-		session = HibernateUtil.getSessionFactory().openSession();
- 
-		try{
+
+	private Session session;
+
+	public void inserir(UF uf) {
+		try {
+			session = HibernateUtil.getSessionFactory().openSession();
 			session.beginTransaction();
 			session.save(uf);
 			session.getTransaction().commit();
- 
-			}finally{
-				session.close(); 
-			}
+
+		} finally {
+			session.close();
 		}
- 
-		public void alterar(UF uf){
+	}
+
+	public void alterar(UF uf) {
+		System.out.println(uf);
+		try {
 			session = HibernateUtil.getSessionFactory().openSession();
-			System.out.println(uf);
-			try{
-				session = HibernateUtil.getSessionFactory().openSession();
-				session.beginTransaction();
-				session.saveOrUpdate(uf);
-				session.getTransaction().commit();
-			}finally{
-				session.close();
- 
-			}
+			session.beginTransaction();
+			session.saveOrUpdate(uf);
+			session.getTransaction().commit();
+		} finally {
+			session.close();
+
 		}
- 
-		public void excluir(UF uf){
+	}
+
+	public void excluir(UF uf) {
+		System.out.println(uf);
+		try {
 			session = HibernateUtil.getSessionFactory().openSession();
-			System.out.println(uf);
-			try{
-				session = HibernateUtil.getSessionFactory().openSession();
-				session.beginTransaction();
-				session.delete(uf);
-				session.getTransaction().commit();
- 
-			}finally{
-				session.close();
- 
-			}
+			session.beginTransaction();
+			session.delete(uf);
+			session.getTransaction().commit();
+
+		} finally {
+			session.close();
+
 		}
- 
-		
-		@SuppressWarnings("rawtypes")
-		public List listar(){
+	}
+
+	@SuppressWarnings("rawtypes")
+	public List listar() {
+
+		try {
 			session = HibernateUtil.getSessionFactory().openSession();
- 
-			try{
-				Criteria cri = session.createCriteria(UF.class);
-				return cri.list();
-			}finally{
-				session.close();
- 
-			}
+			Criteria cri = session.createCriteria(UF.class);
+			return cri.list();
+		} finally {
+			session.close();
+
 		}
+	}
 }
