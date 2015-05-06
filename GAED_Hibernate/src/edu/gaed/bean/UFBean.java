281f8158a5d1857fbf028087d3932597cca77ddc
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-05-06 12:56:48 +0000

    - Adicionada mensagens de confirmação (growl) quando há o cadastro,alteração,exclusão e campos vazios : pagina aplicada - uf.xhtml, será replicada nos demais

--- GAED_Hibernate/src/edu/gaed/bean/UFBean.java
+++ GAED_Hibernate/src/edu/gaed/bean/UFBean.java
@@ -3,10 +3,14 @@ package edu.gaed.bean;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
  
+
+
 
 
 
@@ -40,19 +44,27 @@ public class UFBean {
 	@SuppressWarnings("unchecked")
 	public void cadastrar(ActionEvent actionEvent){
 		new UFDao().inserir(uf);
+		FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Cadastrado",  "Unidade Federativa: " + uf.getNome() + " cadastrado com sucesso") );
 		ufs = new UFDao().listar();
 		uf = new UF();
+		
+		
 	}
  
 	@SuppressWarnings("unchecked")
 	public void alterar(){
 		new UFDao().alterar(uf);
+		FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Alterado",  "Unidade Federativa: " + uf.getNome() + " alterada com sucesso") );
 		ufs = new UFDao().listar();
 		uf = new UF();
 	}
 	@SuppressWarnings("unchecked")
 	public void excluir(UF ufex){
 		new UFDao().excluir(ufex);
+		FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Excluido",  "Unidade Federativa: " + ufex.getNome() + " excluida com sucesso") );
 		ufs = new UFDao().listar();
 		uf = new UF();
 	}
