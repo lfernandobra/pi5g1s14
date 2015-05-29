84acb4ddc46457aba33e9715fcd3582a181df942
paulabia80 <paulabia80@gmail.com>
2015-05-29 02:37:21 +0000

    Testar query do criteria para listar somente os alunos sem turma

--- GAED_Hibernate/src/edu/gaed/dao/AlunoDao.java
+++ GAED_Hibernate/src/edu/gaed/dao/AlunoDao.java
@@ -2,9 +2,12 @@ package edu.gaed.dao;
 
 import java.util.HashSet;
 import java.util.Set;
+
 import edu.gaed.vo.Aluno;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import edu.gaed.util.HibernateUtil;
  
@@ -66,5 +69,21 @@ public class AlunoDao {
 				session.close();
  
 			}
+		}
+		
+		@SuppressWarnings({ "rawtypes", "unchecked" })
+		public Set listaAlunoSemTurma(){
+			session = HibernateUtil.getSessionFactory().openSession();
+			try{
+				Criteria cri = session.createCriteria(Aluno.class);
+				cri.add(Restrictions.isNull("turma")); 
+				return new HashSet (cri.list());
+			}finally{
+				session.close();
+				
+			}
+			
+
+			
 		}
 }
