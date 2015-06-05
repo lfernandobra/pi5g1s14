0aa4dbf0b8b9ba69afd6721842763a36c1db9971
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-06-05 14:54:18 +0000

    - Desenvolvimento do boletim (inserir boletim á turma de acordo com os alunos e disciplinas)

--- GAED_Hibernate/src/edu/gaed/bean/BoletimAlunoDisciplinaBean.java
+++ GAED_Hibernate/src/edu/gaed/bean/BoletimAlunoDisciplinaBean.java
@@ -1,13 +1,16 @@
 package edu.gaed.bean;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.event.ActionEvent;
 
+import edu.gaed.vo.Aluno;
 import edu.gaed.vo.BoletimAlunoDisciplina;
+import edu.gaed.vo.Disciplina;
 import edu.gaed.vo.Turma;
 import edu.gaed.dao.BoletimAlunoDisciplinaDao;
  
@@ -15,78 +18,81 @@ import edu.gaed.dao.BoletimAlunoDisciplinaDao;
 @ViewScoped
 public class BoletimAlunoDisciplinaBean {
  
-	BoletimAlunoDisciplina boletimalunodisciplina = new BoletimAlunoDisciplina();
-	 
-	List<BoletimAlunoDisciplina> boletimalunodisciplinas = new ArrayList<BoletimAlunoDisciplina>(); 
-	
-	public BoletimAlunoDisciplinaBean(BoletimAlunoDisciplina boletimalunodisciplina, List<BoletimAlunoDisciplina> boletimalunodisciplinas) {
-		super();
-		this.boletimalunodisciplina = new BoletimAlunoDisciplina();
-		this.boletimalunodisciplinas = new ArrayList<BoletimAlunoDisciplina>();
-	}
+        private BoletimAlunoDisciplina boletimalunodisciplina = new BoletimAlunoDisciplina();
+        private List<BoletimAlunoDisciplina> boletimalunodisciplinas = new ArrayList<BoletimAlunoDisciplina>(); 
+        private Turma turma;
+        
+        public BoletimAlunoDisciplinaBean(BoletimAlunoDisciplina boletimalunodisciplina, List<BoletimAlunoDisciplina> boletimalunodisciplinas) {
+                super();
+                this.boletimalunodisciplina = new BoletimAlunoDisciplina();
+                this.boletimalunodisciplinas = new ArrayList<BoletimAlunoDisciplina>();
+        }
  
-	//construtor
-	@SuppressWarnings("unchecked")
-	public BoletimAlunoDisciplinaBean(){
-		boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
-		boletimalunodisciplina = new BoletimAlunoDisciplina();
-	}
+        //construtor
+        @SuppressWarnings("unchecked")
+        public BoletimAlunoDisciplinaBean(){
+                boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
+                boletimalunodisciplina = new BoletimAlunoDisciplina();
+        }
  
-	//Métodos dos botões 
-	@SuppressWarnings("unchecked")
-	public void cadastrar(ActionEvent actionEvent){
-		new BoletimAlunoDisciplinaDao().inserir(boletimalunodisciplina);
-		boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
-		boletimalunodisciplina = new BoletimAlunoDisciplina();
-		
-		
-	}
-	/*
-	public void cadastrar(Turma turma){
-		for(int i=0;i<turma.getAlunos().size();i++){
-			boletimalunodisciplina.setAluno(turma.getAlunos().contains(i).);
-			
+        //Métodos dos botões 
+        public void cadastrar(ActionEvent actionEvent){
+                        			
+        	for(Iterator<Aluno> iterAluno = turma.getAlunos().iterator(); iterAluno.hasNext();) 
+        	{ 
+        		Aluno alunoAtual = iterAluno.next(); 
+        		boletimalunodisciplina.setAluno(alunoAtual);
+        		
+        		for(Iterator<Disciplina> iterDisciplina = turma.getDisciplinas().iterator(); iterDisciplina.hasNext();)
+        		{
+        			Disciplina disciplinaAtual = iterDisciplina.next();
+        			boletimalunodisciplina.setDisciplina(disciplinaAtual);
+        			new BoletimAlunoDisciplinaDao().inserir(boletimalunodisciplina);
+        		}
+        	}
+        }
+        
+        @SuppressWarnings("unchecked")
+        public void alterar(){
+                new BoletimAlunoDisciplinaDao().alterar(boletimalunodisciplina);
+                boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
+                boletimalunodisciplina = new BoletimAlunoDisciplina();
+        }
+        @SuppressWarnings("unchecked")
+        public void excluir(BoletimAlunoDisciplina boletimalunodisciplinaex){
+                new BoletimAlunoDisciplinaDao().excluir(boletimalunodisciplinaex);
+                boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
+                boletimalunodisciplina = new BoletimAlunoDisciplina();
+        }
+        
+        //getters and setters
+        
+
+        public List<BoletimAlunoDisciplina> getBoletimAlunoDisciplinas() {
+                return boletimalunodisciplinas;
+        }
+
+        public void setBoletimAlunoDisciplinas(List<BoletimAlunoDisciplina> boletimalunodisciplinas) {
+                this.boletimalunodisciplinas = boletimalunodisciplinas;
+        }
+
+        public BoletimAlunoDisciplina getBoletimAlunoDisciplina() {
+                return boletimalunodisciplina;
+        }
+
+        public void setBoletimAlunoDisciplina(BoletimAlunoDisciplina boletimalunodisciplina) {
+                this.boletimalunodisciplina = boletimalunodisciplina;
+        }
+
+		public Turma getTurma() {
+			return turma;
 		}
-		new BoletimAlunoDisciplinaDao().inserir(boletimalunodisciplina);
-		boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
-		boletimalunodisciplina = new BoletimAlunoDisciplina();
-		
-		
-	}
- 	*/
-	@SuppressWarnings("unchecked")
-	public void alterar(){
-		new BoletimAlunoDisciplinaDao().alterar(boletimalunodisciplina);
-		boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
-		boletimalunodisciplina = new BoletimAlunoDisciplina();
-	}
-	@SuppressWarnings("unchecked")
-	public void excluir(BoletimAlunoDisciplina boletimalunodisciplinaex){
-		new BoletimAlunoDisciplinaDao().excluir(boletimalunodisciplinaex);
-		boletimalunodisciplinas = new BoletimAlunoDisciplinaDao().listar();
-		boletimalunodisciplina = new BoletimAlunoDisciplina();
-	}
-	
-	//getters and setters
-	
 
-	public List<BoletimAlunoDisciplina> getBoletimAlunoDisciplinas() {
-		return boletimalunodisciplinas;
-	}
-
-	public void setBoletimAlunoDisciplinas(List<BoletimAlunoDisciplina> boletimalunodisciplinas) {
-		this.boletimalunodisciplinas = boletimalunodisciplinas;
-	}
-
-	public BoletimAlunoDisciplina getBoletimAlunoDisciplina() {
-		return boletimalunodisciplina;
-	}
-
-	public void setBoletimAlunoDisciplina(BoletimAlunoDisciplina boletimalunodisciplina) {
-		this.boletimalunodisciplina = boletimalunodisciplina;
-	}
+		public void setTurma(Turma turma) {
+			this.turma = turma;
+		}
  
-	// métodos reset
-	
-	
+        // métodos reset
+        
+        
 }
