c8aaa52f819e2e33b22c012a6320f7204616ad9d
lfernandobra <lfernandobra@69287281-2bc0-825f-bd21-3fae9f1d9539>
2015-06-09 00:47:46 +0000

    - Alterações referentes ao faces-config.xml e correção da autenticação da secretária

--- trunk/GAED_Hibernate/src/edu/gaed/vo/Disciplina.java
+++ trunk/GAED_Hibernate/src/edu/gaed/vo/Disciplina.java
@@ -45,7 +45,7 @@ public class Disciplina implements Serializable{
 	@Column(name="conteudo")
 	private String conteudo;
 	
-	@ManyToMany(fetch = FetchType.LAZY)
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(name = "disciplina_professor",
 	joinColumns = { @JoinColumn(name = "iddisciplina", nullable = false,
 	updatable = false) }, inverseJoinColumns = {
